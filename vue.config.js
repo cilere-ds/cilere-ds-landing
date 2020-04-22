17e8d6b301c6389540a01d66fd30ab7f408a43af
cto <cto@cilere.com>
2020-04-22 23:27:05 +0200

    up

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+  pwa: {
+    name: "priviledger",
+    themeColor: "#2d3748",
+    msTileColor: "#2d3748",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
+};
