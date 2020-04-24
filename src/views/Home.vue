4b0a0d9ce0c1d73f52d45757a1acf34e97ba5b31
cto <cto@cilere.com>
2020-04-24 11:53:49 +0200

    up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,16 @@
     style="background: #1d1626 url(/img/background.png) no-repeat center center;"
   >
     <div class="m-auto">
-      <div class="text-white font-bold text-xl">Welcome!</div>
+      <div class="text-white font-normal text-xl">Welcome!</div>
       <div class="text-white font-bold text-xl py-2">丝蓝数码策略</div>
       <div class="text-white font-black text-3xl py-2">
         CILERE Digital Strategy
       </div>
       <div
-        class="rounded rounded-t-lg overflow-hidden shadow w-full px-10 my-3 bg-white"
+        @click="toPriviledger"
+        class="cursor-pointer rounded rounded-t-lg overflow-hidden w-full px-10 my-3 bg-white text-grey hover:text-blue"
       >
-        <p class="mt-2 font-sans font-bold text-grey-dark">
+        <p class="mt-2 font-normal text-xs lg:text-base xl:text-xl">
           Thank you for your interest in our flagship project
         </p>
         <div class="flex justify-center">
@@ -23,16 +24,16 @@
           />
         </div>
         <div class="text-center px-3 pb-6 pt-2">
-          <h3 class="text-black text-sm bold font-sans">贝维悠悦</h3>
-          <p class="mt-2 font-sans font-bold text-grey-dark">
+          <h3 class="text-sm font-medium font-sans">贝维悠悦</h3>
+          <p class="mt-2 font-sans font-medium ">
             P R I V I L E D G E R
           </p>
-          <p class="mt-2 text-xs font-light text-grey-dark">
+          <p class="mt-2 text-xs font-light">
             TREASURES AROUND THE GLOBAL
           </p>
         </div>
-        <div class="flex justify-center pb-3 text-grey-dark">
-          <div>latest XXX from Paris, London, Milan ?? more</div>
+        <div class="flex justify-center font-normal pb-3">
+          <div>Lastest trends from Paris, London, Milan & more</div>
         </div>
       </div>
     </div>
@@ -41,6 +42,11 @@
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  methods: {
+    toPriviledger() {
+      window.location.href = "https://www.priviledger.fr/";
+    }
+  }
 };
 </script>
